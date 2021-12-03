d52996fae233fceefedb298ee3e34d68a513f553
ShiftGuazz <bjn_99@sbcglobal.net>
2021-12-03 11:26:36 -0800

    hmm lets see if this works

---
+++ Day-1/sonar-sweep.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+//pull in the helper functions (somehow)
+
+pub fn main() !u32{
+  //pull file, and read each line from the file
+  var text_sonar : [6]u8 {""}
+  var sonar_value : u32 = undefined;
+
+  
+  
+}
---
+++ helpers.zig
@@ -0,0 +1,7 @@
+
+///function that pulls input from a text file at a day #
+/// returns an... iterable? maybe.
+/// TODO: get this documention better - this will improve as I understand the language better
+pub fn getInput(day: u8){
+
+}
