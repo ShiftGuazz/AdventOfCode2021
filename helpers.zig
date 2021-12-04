1728bafd030c5bc9a898e910a615c913af46605e
ShiftGuazz <bjn_99@sbcglobal.net>
2021-12-03 16:06:14 -0800

    please let this work...

--- helpers.zig
+++ helpers.zig
@@ -3,5 +3,6 @@
 /// returns an... iterable? maybe.
 /// TODO: get this documention better - this will improve as I understand the language better
 pub fn getInput(day: u8){
-
+  //just a small change for testing
+  
 }
