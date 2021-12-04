4da3ab8c3c41e7ebe6f318e186c5113a3fbd5587
ShiftGuazz <bjn_99@sbcglobal.net>
2021-12-03 16:21:16 -0800

    added description of the getInput function

--- helpers.zig
+++ helpers.zig
@@ -1,8 +1,11 @@
 
 ///function that pulls input from a text file at a day #
-/// returns an... iterable? maybe.
+/// returns a 2d array of u8s, one per line.
 /// TODO: get this documention better - this will improve as I understand the language better
 pub fn getInput(day: u8){
-  //just a small change for testing
+  //open the input file associated with day
+  //if the file does not exist, get the input file from adventofcode2021
+  //cache the file
+  //read each line in the file and place each line into the returned 2d array
   
 }
